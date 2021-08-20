7bbb927429795cf668c3f4d8b53dc5e8e060de51
Minki <rlaalsrl9569@naver.com>
2021-08-20 22:22:43 +0900

    POST 기능 추가

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ const logger = morgan("dev");
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "pug");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);

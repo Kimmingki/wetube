aee94a1b69584bdaaedecc477f557b39544cbc04
Minki <rlaalsrl9569@naver.com>
2021-08-26 23:37:13 +0900

    Exceptions and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,18 +29,21 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags
-      .split(",")
-      .map((word) => `#${word}`)
-      .map((word) => word.trim()),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags
+        .split(",")
+        .map((word) => `#${word}`)
+        .map((word) => word.trim()),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pagTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };

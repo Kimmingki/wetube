c7353ffd1530bb7cdacf6de1c7828d7ecf0e0b45
Minki <rlaalsrl9569@naver.com>
2021-08-30 19:41:03 +0900

    statics and delete video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,7 +42,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -57,7 +57,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
@@ -67,4 +67,10 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndUpdate(id);
+  return res.redirect("/");
 };

55b9c9ad192e19fad408f9acc83dfea76884ce19
Minki <rlaalsrl9569@naver.com>
2021-08-30 18:02:12 +0900

    미들웨어

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -35,16 +35,14 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exist({ _id: id });
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startWith("#") ? word : `#${word}`)),
+    hashtags,
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -59,9 +57,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startWith("#") ? word : `#${word}`)),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {

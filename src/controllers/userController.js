95233b8f6204c656b469b8e7607a7176166cd66f
Minki <rlaalsrl9569@naver.com>
2021-08-31 22:00:32 +0900

    Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,24 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  // password가 같은지 확인
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  // 이미 사용중인 데이터 알림
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username or email is already taken.",
+    });
+  }
   // req.body에서 데이터 가져와서 user 생성하기
   await User.create({
     name,

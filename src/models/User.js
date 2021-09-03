8ec9f00f6ca29c0b8b115080964187b6bb2a77e6
Minki <rlaalsrl9569@naver.com>
2021-09-03 12:05:10 +0900

    github 로그인, 회원가입 만들기

--- src/models/User.js
+++ src/models/User.js
@@ -4,8 +4,9 @@ import mongoose from "mongoose";
 // user 모델 만들기
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { edit, remove, profile, logout } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  profile,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", profile);
 
 export default userRouter;

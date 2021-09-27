f77a515914301f8de3580ff9d612c32a6e8fbf78
Minki <rlaalsrl9569@naver.com>
2021-09-27 23:04:20 +0900

    파일 업로드시 비밀번호가 해쉬되는 버그 수정

--- src/models/User.js
+++ src/models/User.js
@@ -15,7 +15,9 @@ const userSchema = new mongoose.Schema({
 
 // password 암호화 하기
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);

cf92ca37704967aeb9c8eaa9c92867c5eedf9160
Minki <rlaalsrl9569@naver.com>
2021-09-30 22:27:22 +0900

    MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   // webpack.config.js 필수 사항 (entry, output)
   entry: "./src/client/js/main.js",
+  // JS 코드에서 CSS를 분리하기 위해서
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   // 현재 개발중인지 완성품인지 mode를 적어줌
   mode: "development",
   output: {
-    filename: "main.js",
+    filename: "js/main.js",
     // output path는 절대경로로 적어야함
-    path: path.resolve(__dirname, "assets", "js"),
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -24,7 +31,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },

40bc54bb31cc800bfd1069397259981b43d966a9
Minki <rlaalsrl9569@naver.com>
2021-10-04 16:39:47 +0900

    Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,9 +2,13 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
-const handlePlayClick = (e) => {
+let volume = 0.5;
+
+video.volume = volume;
+
+const handlePlay = (e) => {
   // 비디오가 실행중이면 멈추게 하기
   if (video.paused) {
     video.play();
@@ -12,14 +16,31 @@ const handlePlayClick = (e) => {
     // 실행중이 아니면 실행하기
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volume;
+};
 
-const handleMute = (e) => {};
+const handleVolumeChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volume = value;
+  video.volume = value;
+};
 
-playBtn.addEventListener("click", handlePlayClick);
+playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input", handleVolumeChange);

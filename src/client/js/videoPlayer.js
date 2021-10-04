5307250a0d82e8e29c4fa00f8d36e5e3e2239c9f
Minki <rlaalsrl9569@naver.com>
2021-10-04 16:12:42 +0900

    Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,25 @@
-console.log("video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  // 비디오가 실행중이면 멈추게 하기
+  if (video.paused) {
+    video.play();
+  } else {
+    // 실행중이 아니면 실행하기
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
